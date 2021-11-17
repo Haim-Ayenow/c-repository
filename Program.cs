9edfe3d78d93dade5a1ad05cbad8f83230bed590
haimaye074@gmail.com <haimaye074@gmail.com>
2021-11-17 13:16:26 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Hello World!");//1
+            //Console.WriteLine("haim ayenow");//2
+            //string name=Console.ReadLine();//3
+            //Console.WriteLine(name);
+
+
+            //Console.WriteLine(5);//4
+
+            //int num1 = int.Parse(Console.ReadLine());//5
+            //Console.WriteLine(num1);
+
+
+
+            //int num2 = int.Parse(Console.ReadLine());//6
+            //if (num2 > 25)
+            //{
+            //    Console.WriteLine("hi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bye");
+            //}
+
+
+
+            //int num3 = int.Parse(Console.ReadLine());//7
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(num3);
+            //}
+
+            //int num4 = int.Parse(Console.ReadLine());//8
+            //for (int i = 0; i < num4; i++)
+            //{
+            //    Console.WriteLine(num4);
+            //}
+
+
+
+            //string name = Console.ReadLine();//9
+            //int num5 = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < num5; i++)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //int num = 1;//10
+            //int num1 = 2;
+            //int num2 = 3;
+            //int num3 = 4;
+            //double avg = (num + num1 + num2 + num3);
+            //double sum = (avg / 4);
+            //Console.WriteLine(avg);
+            //Console.WriteLine(sum);
+
+
+            //int length = 50;//11
+            //int weight = 80;
+            //Console.WriteLine(length * weight);
+
+            // int num1 = 3;//12
+            // int num2 = 4;
+            // Console.WriteLine(num1);
+            // Console.WriteLine(num2);
+            // int num3 = num2;
+            //  num2 = num1;
+            // num1 = num3;
+            // Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+
+            //int num = int.Parse(Console.ReadLine());//13
+            //Console.WriteLine(num);
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num1);
+
+            //int num = int.Parse(Console.ReadLine());//14
+            //Console.WriteLine(num);
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num1);
+
+            //string a1 = "a";//15
+            //string a2 = "1";
+            //Console.WriteLine(a1);
+            //Console.WriteLine(a2);
+
+            //bool a = true;//16
+            //Console.WriteLine(a);
+
+            //int num = 3;//17
+            //int num1 = 4;
+            //int num2 = 5;
+            //int mul = (num * num1 * num2);
+            //Console.WriteLine(mul);
+
+
+
+            //float first = float.Parse(Console.ReadLine());//18
+            //float second = float.Parse(Console.ReadLine());
+
+            //float result = first % second;
+
+            //Console.WriteLine(result);
+
+            //int i = 0;
+            //int multi = 1;
+            //while (i < 3)
+            //{
+            //    multi = multi * int.Parse(Console.ReadLine());
+            //    i++;
+            //}
+            //Console.WriteLine(multi);
+
+
+
+            //int number = 1;//19
+            //number++;
+            //Console.WriteLine(number);
+
+
+
+            //int num = 3;//20
+
+            //num *= 3;
+            //Console.WriteLine(num);
+
+
+
+            // int radius = 5;//21
+
+            //double pi = 3.14;
+
+            //double area = pi * radius * radius;
+            //Console.WriteLine(area);
+
+            //double border = 2 * pi * radius;
+            //Console.WriteLine(border);
+
+
+
+
+            //int num = 1;//22
+            //while (num < 101)
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+            //}
+
+
+
+
+
+            //int num = 2;//23
+
+            //while (num < 51)
+            //{
+            //    Console.WriteLine(num);
+            //    num += 2;
+            //}
+
+
+
+            //int num = 1;//24
+
+            //while (num < 51)
+            //{
+            //    Console.WriteLine(num);
+            //    num += 3;
+            //}
+
+
+
+            //  int i = 0;//25
+            //int sum = 0;
+            //while (i < 5)
+            //{
+            //    int num = int.Parse(Console.ReadLine());
+            //    sum += num;
+            //    i++;
+
+            //}
+            //Console.WriteLine(sum);
+
+            //int avg = sum / 5;
+            //Console.WriteLine(avg);
+
+
+
+
+            //int min = int.MaxValue;//26
+            //int max = int.MinValue;
+            //int i = 0;
+            //while (i < 5)
+            //{
+            //    int someNum = int.Parse(Console.ReadLine());
+            //    if (someNum < min) { min = someNum; }
+            //    if (someNum > max) { max = someNum; }
+            //    i++;
+
+            //}
+            //Console.WriteLine(max);
+            //Console.WriteLine(min);
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+   
+        
+   
